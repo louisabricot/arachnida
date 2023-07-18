46b324731b65dd7db6c5677f89992e138ab3f93a
Louisa <45848751+louisabricot@users.noreply.github.com>
2023-07-18 13:29:20 +0200

    Implemented linter action and hooks - Use Black code formatter in pre-commit-hooks - Generate requirements.txt in pre-commit-hooks with pipreqs - Run Pylint on push

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Arachnida
+![linting: pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/pylint-dev/pylint)
 
 Introductory project to web scraping and metadata
 
--- spider.py
+++ spider.py
@@ -1,68 +1,71 @@
-import argparse
+""" Parses and validates binary arguments """
+
 import sys
+import argparse
 import validators
 
 LEVEL_MIN_VAL = 1
 LEVEL_MAX_VAL = 100
 
+
 def url_type(arg):
-    """ Type function for argparse - validates the url """ 
-    
+    """Type function for argparse - validates the url"""
+
     validation = validators.url(arg, public=True)
     if validation:
         return arg
-    else:
-        raise argparse.ArgumentTypeError("Must be a valid url")
+    raise argparse.ArgumentTypeError("Must be a valid url")
+
 
 def range_limited_int_type(arg):
-    """ Type function for argparse - an integer within some predefined bounds """
+    """Type function for argparse - an integer within some predefined bounds"""
 
     try:
-        n = integer(arg)
-    except ValueError:
-        raise argparse.ArgumentTypeError("Must be an integer number")
-    if n < LEVEL_MIN_VAL or n > LEVEL_MAX_VAL:
-        raise argparse.ArgumentTypeError("Argument must be < " +
-        str(LEVEL_MAX_VAL) + "and > " + str(LEVEL_MIN_VAL))
-    return n
-        
+        num = int(arg)
+    except ValueError as exc:
+        raise argparse.ArgumentTypeError("Must be an integer number") from exc
+    if num < LEVEL_MIN_VAL or num > LEVEL_MAX_VAL:
+        raise argparse.ArgumentTypeError(
+            "Argument must be < " + str(LEVEL_MAX_VAL) + "and > " + str(LEVEL_MIN_VAL)
+        )
+    return num
+
+
 def main():
+    """Extracts images from the url provided as argument"""
     parser = argparse.ArgumentParser(
-                        description='Extracts images from the provided url',
-                        epilog='Developed by louisabricot')
-    
-    parser.add_argument(
-        'url',
-        type=url_type,
-        help='The website\'s url'
+        description="Extracts images from the provided url",
+        epilog="Developed by louisabricot",
     )
-    
+
+    parser.add_argument("url", type=url_type, help="The website's url")
+
     parser.add_argument(
-        '--recursive',
-        '-r',
-        action='store_true',
-        required='-l' in sys.argv,
-        help='Recursive download'
-    )
-    
-    parser.add_argument(
-        '--level',
-        '-l',
-        type=range_limited_int_type,
-        default=5,
-        help='The maximum depth level of the recursive download. Default is 5'
+        "--recursive",
+        "-r",
+        action="store_true",
+        required="-l" in sys.argv,
+        help="Recursive download",
     )
 
     parser.add_argument(
-        '-p',
-        type=str,
-        default='./data/',
-        help='The path where the downloaded files will be saved. Default is ./data/'
+        "--level",
+        "-l",
+        type=range_limited_int_type,
+        default=5,
+        help="The maximum depth level of the recursive download. Default is 5",
     )
-    
+
+    parser.add_argument(
+        "-p",
+        type=str,
+        default="./data/",
+        help="The path where the downloaded files will be saved. Default is ./data/",
+    )
+
     args = parser.parse_args()
-    print('Hello ' + args.url + 'level: ' + repr(args.level), 'data storage ' +
-    args.p)
+    print("Hello " + args.url + "level: " + repr(args.level), "data storage " + args.p)
+
 
 if __name__ == "__main__":
     main()
