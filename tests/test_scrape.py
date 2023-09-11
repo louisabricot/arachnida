d9c6bb994e7314d07ab1dad409b5f1fc165d9d0f
louisabricot <louisabricot@protonmail.com>
2023-09-11 17:37:01 +0200

    Added some ideas for test cases

---
+++ tests/test_scrape.py
@@ -0,0 +1,11 @@
+""" TODO: check response with no content-type
+          400, redirections
+          already visited redirection
+          not visited redirection
+          200
+          200 with XML page
+          200 that is not a text/html page
+          200 that is a text/html page
+          
+
+"""
