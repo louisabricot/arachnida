022f20c4555c060c4354be5993368eae9ac5bfc0
louisabricot <louisabricot@protonmail.com>
2023-09-11 17:36:21 +0200

    Clean code, docstrings and fixed some logical bugs

--- srcs/spider/scrape.py
+++ srcs/spider/scrape.py
@@ -7,86 +7,103 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import logging
+import validators
 
-def scrape_images(page_url: str, image_url_patterns: str) -> None:
+
+def scrape_images(webpage: str, image_url_patterns: str) -> set:
     """
-    Scrape images from the HTML page URL that match the given image URL
+    Scrapes image URLs from a webpage matching the specified image URLs
     patterns.
 
-    Parameters:
-        page_url (str): The URL of the HTML page to scrape for images.
-        image_url_patterns (str): Regular expression pattern to match the image
-        URLS.
+    Args:
+        webpage (str): The URL of the web page to scrape.
+        image_url_patterns (str): A regular expression pattern to filter image
+        URLs
 
     Returns:
-        list: A list of unique image URLs that match the given patterns, or
-        None if an error occurs.
+        set: A set of filtered image URLs matching the pattern.
+            Returns an empty set if no matches are found or an error occurs.
     """
+
     try:
-        # Make an HTTP get request to the page url
-        response = requests.get(page_url, allow_redirects=True)
+        # Make an HTTP GET request to the page URL
+        response = requests.get(webpage, allow_redirects=True)
         response.raise_for_status()
 
         # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(response.content, "html.parser")
 
-        # Find all iamge tags with valid 'src' attribute
-        img_tags = soup.find_all("img", src=True)
+        # Gathers the 'src' attribute of all img tags
+        img_srcs = set(img.attrs["src"] for img in soup.find_all(["img"], src=True))
 
-        # Extract image URLs that match the provided patterns
-        image_urls = []
-        for img in img_tags:
-            match = re.search(image_url_patterns, img["src"])
-            if match:
-                extracted_url = match.group()
-                image_urls.append(extracted_url)
+        # Filters 'src' based on the provided pattern
+        filtered_urls = {src for src in img_srcs if re.search(image_url_patterns, src)}
 
-        # Remove duplicates from list
-        image_urls = list(dict.fromkeys(image_urls))
-        return image_urls
+        return filtered_urls
 
     except requests.exceptions.RequestException as e:
-        log_download_error(page_url, e)
-    return None
+        logging.error(f"HTTP Error {response.status_code} for URL: {webpage}")
+
+    return set()
 
 
-def fetch_url_content(url: str, base_url: str, depth: int, visited: set) -> str:
-    
+def get_urls_from_page(
+    webpage: str, base_url: str, depth: int, visited: set, nested_urls
+) -> set():
+    # Returns if the url is not valid
+    if not validators.url(webpage):
+        return set()
+
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            if response.headers and 'html' in response.headers.get('content-type'):
-                return response.text
-            else:
-                logging.error(f"Cannot parse content-type {response.headers['content-type']} for URL: {url}")
+        # Make an HTTP GET request to the page URL
+        response = requests.get(webpage)
+        response.raise_for_status()
 
+        if response.status_code == 200:
+            # Only scrapes HTML pages
+            if response.headers and "text/html" in response.headers.get("content-type"):
+                nested_urls.add(webpage)
+                print(webpage)
+                return get_urls_from_page_content(
+                    response.text, webpage, base_url, depth
+                )
+            else:
+                logging.error(
+                    f"Cannot parse content-type {response.headers['content-type']} for URL: {webpage}"
+                )
+
+        # Fetch content if redirection within scope
         elif response.status_code in (301, 302, 307, 308):
-            redirection_url = response.headers.get('Location')
-            if redirection_url and url_in_scope(redirection_url, base_url,depth):
+            redirection_url = response.headers.get("Location")
+            if redirection_url and url_in_scope(redirection_url, base_url, depth):
                 if redirection_url not in visited:
-                    fetch_url_content(redirection_url, base_url, depth, visited)
+                    get_urls_from_page(
+                        redirection_url, base_url, depth, visited, nested_urls
+                    )
+
+                    # Add redirection URL to visited set
+                    visited.add(redirection_url)
             else:
                 logging.error(f"Redirection to an external URL: {redirection_url}")
-        else:
-            logging.error(f"HTTP Error {response.status_code} for URL: {url}")
     except requests.RequestException as e:
-        logging.error(f"Request Exception: {str(e)} for URL: {url}")
+        logging.error(f"Request Exception: {str(e)}")
 
-    return None
+    return set()
 
-def retrieve_urls_from_page(content: str, url_page: str, base_url: str, depth: int) -> set:
 
+def get_urls_from_page_content(
+    content: str, webpage: str, base_url: str, depth: int
+) -> set:
     subpaths = set()
     if content:
-        if not re.search(r'<!DOCTYPE html>', content):
-            logging.error(f"Not HTML for URL: {url_page}")
+        if not re.search(r"<!DOCTYPE html>", content):
+            logging.error(f"Not HTML for URL: {webpage}")
             return set()
-        # TODO: Check if file starts with DOCTYPE 
+        # TODO: Check if file starts with DOCTYPE
         soup = BeautifulSoup(content, "html.parser")
         # Get all links from a and link
-        for link in soup.find_all(['link', 'a'], href=True):
-
-            url = clean_url(url_page, link.get('href'))
+        for link in soup.find_all(["link", "a"], href=True):
+            url = clean_url(webpage, link.get("href"))
             if url.startswith(base_url) and url_in_scope(url, base_url, depth):
                 subpaths.add(url)
     return subpaths
@@ -103,17 +120,17 @@ def retrieve_nested_urls(base_url: str, depth: int) -> set:
         return set([base_url])
 
     visited = set()
+    nested_urls = set()
     to_visit = set([base_url])
 
     while to_visit:
-
-        current_url = to_visit.pop()
-        content = fetch_url_content(current_url, base_url, depth, visited)
-        if content:
-            retrieved_urls = retrieve_urls_from_page(content, current_url, base_url, depth)
-            for url in retrieved_urls:
-                to_visit.add(url)
-            visited.add(current_url)
+        webpage = to_visit.pop()
+        retrieved_urls = get_urls_from_page(
+            webpage, base_url, depth, visited, nested_urls
+        )
+        if retrieved_urls:
+            to_visit.update(retrieved_urls)
+        visited.add(webpage)
         to_visit.difference_update(visited)
 
-    return visited
+    return nested_urls
